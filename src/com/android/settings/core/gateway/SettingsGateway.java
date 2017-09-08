f64ee3478c89852b4640f4bcf3689e134c40d3b8
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate GeneraSettings into Settings
    [jaysonedson: Ported to android11 and Android.bp]
    
    Change-Id: I7065daa62396beac47bae88bf7e932a2a93d9b1c
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -169,6 +169,8 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherClientManager;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import org.genera.settings.GeneraSettingsFragment;
+
 public class SettingsGateway {
 
     /**
@@ -325,7 +327,8 @@ public class SettingsGateway {
             NetworkSelectSettings.class.getName(),
             AlarmsAndRemindersDetails.class.getName(),
             MediaManagementAppsDetails.class.getName(),
-            WifiTetherClientManager.class.getName()
+            WifiTetherClientManager.class.getName(),
+            GeneraSettingsFragment.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -366,5 +369,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.GeneraSettingsFragmentActivity.class.getName(),
     };
 }
